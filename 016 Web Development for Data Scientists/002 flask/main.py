94aa0a48e8211d6e972697e6b9cfb235c9aff31e
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-21 14:57:03 +0530

    basic flask app

---
+++ 016 Web Development for Data Scientists/002 flask/main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/about")
+def about():
+    return "<p>This is the about page.</p>"
+
+app.run(debug=True)
