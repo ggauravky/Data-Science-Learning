10eec51887232256b444476164c5b6b4960c0c10
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-22 21:55:26 +0530

    Static and Templates folder

--- 016 Web Development for Data Scientists/002 flask/main.py
+++ 016 Web Development for Data Scientists/002 flask/main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -9,5 +9,8 @@ def hello_world():
 @app.route("/about")
 def about():
     return "<p>This is the about page.</p>"
+@app.route("/contact")
+def contact():
+    return render_template("index.html")
 
 app.run(debug=True)
