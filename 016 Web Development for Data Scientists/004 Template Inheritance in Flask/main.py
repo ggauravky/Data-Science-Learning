51118940b7eea1b18906fcf1e9c82cbdcc4736b4
Gaurav Kumar Yadav <kumar.gaurav.yadav2007@gmail.com>
2026-07-27 17:21:44 +0530

    Message Flashing in Flask

--- 016 Web Development for Data Scientists/004 Template Inheritance in Flask/main.py
+++ 016 Web Development for Data Scientists/004 Template Inheritance in Flask/main.py
@@ -1,9 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template , flash
 
 app=Flask(__name__)
 
+app.secret_key = 'your_secret_key'  # Replace with your actual secret
+
 @app.route('/')
 def hello_world():
+    flash('Welcome to the Flask app!', 'info')
     return render_template('index.html')
 
-app.run(debug=True)
+app.run(debug=True) 
